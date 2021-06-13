edae018c84d9067fa321396db59ac96bfb188df8
dito245 <ditowijaya356@gmail.com>
2021-06-13 14:55:56 +0700

    phpmvc

--- application/views/about/about.php
+++ application/views/about/about.php
@@ -1,5 +1,5 @@
 <div class="container">
     <h1 class="mt-4">About Me</h1>
-    <img src="<?=base_url('img/Mikasa.jpg') ?>" width="200px" class="rounded-circle shadow">
-    <P>Hallo nama saya <?= $nama ?></P>
+    <img src="<?=base_url('img/Band.png') ?>" width="200px" class="rounded-circle shadow">
+    <P>Hallo nama saya Dito Wijaya</P>
 </div>
